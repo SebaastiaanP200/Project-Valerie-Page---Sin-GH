da01cd83db0fb5d3ae918e1218c07e418e6adcd4
Sebastián Pérez <sebastiaan.peerez@gmail.com>
2026-04-23 18:49:27 +0200

    Dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -14,3 +14,10 @@ btn__logout.addEventListener("click", async () => {
   window.location.href = "./login.html";
 });
 
+const menu = document.getElementById("menu");
+const sidebar = document.getElementById("sidebar");
+menu.addEventListener("click", () => {
+  sidebar.classList.toggle("sidebar__menu-toggle");
+
+
+})
